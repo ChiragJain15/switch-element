857f2553cacd5960e957bfb725b410cc3368bfb5
chirag <chiragjain150302@gmail.com>
2021-05-13 20:16:36 +0530

    added themes

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const button = document.getElementById("ball");
 const visible = document.querySelectorAll("h1");
-console.log(visible);
+const themes = [
+  [
+    "rgba(252, 92, 156, 0.5)",
+    "rgba(243, 127, 173, 0.6)",
+    "rgba(253, 198, 219, 0.952)",
+  ],
+  ["#f0f696", "#96f7d2", "#fbfdc7"],
+  ["#364f6b", "#324761", "#ff6262ee"],
+];
+let number = 0;
 button.addEventListener("click", () => {
   button.classList.toggle("dot2");
   visible[0].classList.toggle("show");
+  number++;
+  number = number == themes.length ? number % themes.length : number;
+  console.log(number);
+  document.documentElement.style.setProperty("--primary", themes[number][0]);
+  document.documentElement.style.setProperty("--secondary", themes[number][1]);
+  document.documentElement.style.setProperty("--ball", themes[number][2]);
 });
